b27ba42cd40d6a677f19ac2f2a441a9acb40f4f5
Ben Cohen <benjcohen1985@gmail.com>
2020-08-11 17:35:34 +0300

    get and validate jwt token

--- services/auth-services.js
+++ services/auth-services.js
@@ -3,21 +3,46 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const auth = async (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
-  const data = jwt.verify(token, process.env.JWT_SECRET);
-  try {
-    const user = await User.findOne({ _id: data._id, "tokens.token": token });
-    if (!user) {
-      throw new Error();
-    }
-
-    req.user = user;
-    req.token = token;
-    next();
-  } catch (error) {
-    res.status(401).send({ error: "Not authorized" });
+  console.log(req.headers);
+  // find token in headers
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).send("Access was denied");
+  } else 
+  {
+    // validate token
+    const tokenBody = token && token.split(" ")[1];
+    console.log('TOKEN BODY--',tokenBody);
+    
+    jwt.verify(tokenBody, process.env.JWT_SECRET, (err,decoded)=>{
+      if(err){
+        console.log(`JWT error: ${err}`);
+        return res.status(401).send('access denied, yo')
+      }
+      next();
+    });
   }
+  const payload = {
+    email: "123@email.com",
+  };
+  const accessToken = jwt.sign(payload, process.env.JWT_SECRET);
+  res.send(accessToken);
 };
+
+
+// try {
+//   const user = await User.findOne({ _id: data._id, "tokens.token": token });
+//   if (!user) {
+//     throw new Error();
+//   }
+
+//   req.user = user;
+//   req.token = token;
+//   next();
+// } catch (error) {
+//   res.status(401).send({ error: "Not authorized" });
+// }
+
 // const authApiServices = {
 //   comparePasswords(password, hash) {
 //     return bcrypt.compare(password, hash);
