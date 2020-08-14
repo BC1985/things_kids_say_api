b5ab658804d98264d251da4674177e050010ba98
Ben Cohen <benjcohen1985@gmail.com>
2020-08-14 15:23:20 +0300

    Add schema validation Implement password hashing

--- models/users.model.js
+++ models/users.model.js
@@ -10,58 +10,68 @@ const userSchema = new Schema({
     required: true,
     validate: value => {
       if (!validator.isEmail(value)) {
-        throw new Error({ error: "Invalid email address" });
+        throw new Error( "Invalid email address");
       }
     }
   },
   password: {
     type: String,
-    required: true
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true
-      }
-    }
-  ]
-});
-// hash password before saving
-userSchema.pre("save", async function(next) {
-  let user = this;
-  if (user.isModified("password")) {
-    user.password = await bcrypt.hash(user.password, 8);
+    required: true,
+    minlength: [6, 'Must be at least six characters'],
+    maxlength: [72, 'Password cannot be more that 72 characters long' ]
   }
-  next();
 });
+  // hash password before saving
+userSchema.pre("save", async (next) =>{
+  try{
+    let user = this;
+    // Generate salt
+   const salt= await bcrypt.genSalt(10);
+  //  Generate passord hash
+   const passwordHash = await bcrypt.hash(user.password, salt)
+  // assign password to hashed version
+  user.password  = passwordHash;
+    // const isMatch = await bcrypt.compare(passwordHash, user.password);
+    // if (!isMatch) {
+    //     throw new Error({ error: "Invalid login credentials" });
+    // } 
+  next()
+  }
+  catch(error){
+    next(error);
+  }
+
+});
+
+// userSchema.method('isPasswordValid', async (newPassword)=>{
+//   try { 
+//    return await bcrypt.compare(newPassword, user.password)
+//   } catch (error) {
+//     throw new Error(error)
+//   }
+// })
 // search for user by email or password
 userSchema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({ email });
+  const user = await User.findOne({password: password, email: email});
   if (!user) {
-    throw new Error({ error: "Invalid login credentials" });
-  }
-  const isPasswordMatch = bcrypt.compare(password, user.password);
-  if (!isPasswordMatch) {
     throw new Error({ error: "Invalid login credentials" });
   }
   return user;
 };
 // compare passwords
 
-userSchema.methods.comparePassword = (password, done) => {
-  bcrypt.compare(password, this.password, (err, isMatch) => {
-    done(err, isMatch);
-  });
-};
-userSchema.methods.generateAuthToken = async function() {
-  // Generate an auth token for the user
-  const user = this;
-  // const token = JWT.sign({ _id: user._id }, process.env.JWT_SECRET);
-  const token = JWT.sign({ _id: user._id }, process.env.JWT_SECRET);
-  user.tokens = user.tokens.concat({ token });
-  await user.save();
-  return token;
-};
+// const isMatch = userSchema.isPasswordValid(password)
+// const isPasswordMatch = bcrypt.compare(user.password, user.password);
+// if (!isPasswordMatch) {
+//   throw new Error({ error: "Invalid login credentials" });
+// }else{
+//   return user
+// }
+ 
+// userSchema.methods.comparePassword = (password, done) => {
+//   bcrypt.compare(password, this.password, (err, isMatch) => {
+//     done(err, isMatch);
+//   });
+// };
 const User = mongoose.model("User", userSchema);
 module.exports = User;
