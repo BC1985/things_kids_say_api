52303a3cbae7f88153caf801957ba5272ef22f29
Ben Cohen <benjcohen1985@gmail.com>
2020-08-28 10:50:17 +0300

    Modify message text

--- models/users.model.js
+++ models/users.model.js
@@ -9,23 +9,22 @@ mongoose.plugin(require("mongoose-unique-validator"), {
 const userSchema = new Schema({
   email: {
     type: String,
-    required: [true, "Please enter an email"],
+    required: [true, "Please enter email"],
     validate: [isEmail, "Please enter valid email"],
     unique: true,
   },
   password: {
     type: String,
-    required: [true, "Password required"],
+    required: [true, "Password is required"],
     minlength: [6, "Must be at least six characters"],
     maxlength: [72, "Password cannot be more that 72 characters long"],
     validate: {
       validator: val => {
-        var regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[\S]+/;
+        let regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[\S]+/;
         return val == null || val.trim().length < 1 || regex.test(val);
       },
-      message: "Password must include uppercase, lowecase, numbers and symbols",
+      message: "Password must include uppercase, lowecase, numbers and symbols without spaces",
     },
-    // /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[\S]+/
   },
 });
 
