9733b5530768bbb04fa5b61ef7d27aa5975fa3fa
Ben Cohen <benjcohen1985@gmail.com>
2021-04-14 20:24:56 +0300

    Change connection variable name

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.CONNECTION_URI;
+const uri = process.env.MONGO_URL;
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
