cecdae960cd4212a379254e7d20351668b5871de
Ben Cohen <benjcohen1985@gmail.com>
2021-04-14 18:09:27 +0300

    Change connectio string

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.CONNECTION_URL;
+const uri = process.env.CONNECTION_URI;
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
